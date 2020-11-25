756bab6bb25481119421cdbb65c39ab7aa69cb5c
lb-ali <lbalidrive@gmail.com>
2020-11-25 10:04:16 -0500

    Documented code, cycle of size 55 found

--- src/App.java
+++ src/App.java
@@ -8,57 +8,14 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Graph g = new Graph
-
-        // for (int i = 0; i < 5; i++) {
-        // Vertex temp = new Vertex(i);
-        // teams.add(temp);
-        // }
-        // for (int i = 0; i < 4; i++) {
-        // Vertex temp1 = new Vertex(i);
-        // Vertex temp2 = new Vertex(i + 1);
-        // Edge temp = new Edge(temp1, temp2);
-        // games.add(temp);
-        // }
-        // Create a 5-team tournament graph
-        // Vertex one = new Vertex(1);
-        // Vertex two = new Vertex(2);
-        // Vertex three = new Vertex(3);
-        // Vertex four = new Vertex(4);
-        // Vertex five = new Vertex(5);
-        // teams.add(one);
-        // teams.add(two);
-        // teams.add(three);
-        // teams.add(four);
-        // teams.add(five);
-        // games.add(new Edge(two, one));
-        // games.add(new Edge(three, one));
-        // games.add(new Edge(four, one));
-        // games.add(new Edge(one, five));
-
-        // games.add(new Edge(two, three));
-        // games.add(new Edge(two, five));
-        // games.add(new Edge(four, two));
-
-        // games.add(new Edge(three, four));
-        // games.add(new Edge(five, three));
-
-        // games.add(new Edge(four, five));
-
-        // Graph g = new seasonGraph(0, teams, games);
-        // // g.print();
-        // // System.out.println(g.isNeighbor(two, one));
-        // // System.out.println(g.isNeighbor(one, two));
-        // g.hamCycle(three);
         ArrayList<Vertex> teams = new ArrayList<Vertex>();
         ArrayList<Edge> games = new ArrayList<Edge>();
         Dictionary<Integer, String> idLookup = new Hashtable<Integer, String>();
 
+        // Read data from csv file using BufferedReader
         String csv = "/home/lbali/Documents/Java/cfb_project/cfb_project/src/data.csv";
         BufferedReader br = null;
-        String line = "";
         String splitBy = ",";
-        int count = 0;
         try {
             br = new BufferedReader(new FileReader(csv));
             // br.readLine();
@@ -77,9 +34,7 @@ public class App {
 
                 // Check if teams have already been added to vertices
                 Enumeration<Integer> enumerator = idLookup.keys();
-                // while (enumerator.hasMoreElements()) {
                 for (int i = 0; i < teams.size(); i++) {
-                    // int tempID = enumerator.nextElement();
                     int tempID = teams.get(i).id;
                     if (tempID == id1) {
                         team1 = teams.get(i);
@@ -98,11 +53,6 @@ public class App {
                 } else {
                     tempEdge = new Edge(team2, team1);
                 }
-                // if (teams.size() > 41 && (team1.id == teams.get(39).id || team2.id ==
-                // teams.get(39).id)) {
-                // tempEdge.print();
-                // System.out.println(team1 + "; " + team2);
-                // }
                 games.add(tempEdge);
 
                 // Add teams to vertices if necessary
@@ -114,7 +64,6 @@ public class App {
                     teams.add(team2);
                     idLookup.put(team2.id, temp[21]);
                 }
-                count++;
             }
         } finally {
             if (br != null) {
@@ -128,48 +77,51 @@ public class App {
         }
         // System.out.println(count);
 
+        // Initialize a graph which represents all games played in the season
         seasonGraph g = new seasonGraph(0, teams, games, idLookup);
-        int size = 30;
+        // Seed size is how large the initial cycle found will be
+        int seedSize = 10;
+        // Final size is how large the final cycle will be
+        int finalSize = 55;
         int firstSize = 0;
-        int expanded = 0;
         Random rand = new Random();
         int seed = rand.nextInt(teams.size());
 
+        // Initialize an empty path before path finding
         ArrayList<Vertex> path = new ArrayList<Vertex>();
-        while (expanded - firstSize < 5) {
-            if (expanded - firstSize < 5) {
+        while (path.size() < finalSize) {
+            // Find a new random vertex to start the cycle
+            if (path.size() < finalSize) {
                 do {
                     seed = rand.nextInt(teams.size());
-                } while (teams.get(seed).neighbors.size() < 1);
+                } while (teams.get(seed).neighbors.size() < 2);
             }
-            path = g.circle(teams.get(seed), size + 10);
+            // Use try and catch because circle sometimes throws a null pointer exception
+            try {
+                path = g.circle(teams.get(seed), seedSize + 10);
+            } catch (Exception e) {
+                path = new ArrayList<Vertex>();
+            }
 
-            if (path.size() > size) {
+            if (path.size() > seedSize) {
                 firstSize = path.size();
-                System.out.println("Finding subpaths, start path (" + firstSize + " units long) in: ");
-                g.printPath(path);
-                // // Expand by finding unique cycles within this cycle
+                // System.out.println("Finding subpaths, start path (" + firstSize + " units
+                // long) in: ");
+                // g.printPath(path);
+
+                // Expand the cycle by finding unique paths within this cycle
                 for (int i = 1; i < path.size() - 1; i++) {
                     firstSize = path.size();
                     // // Find a cycle starting from this point
                     Vertex temp1 = path.get(i);
                     Vertex temp2 = path.get(i + 1);
-                    // g.vertexPrint(temp1);
-                    // g.vertexPrint(temp2);
+                    // Find the longest path between these two points
                     g.longestPath(temp1, temp2, path);
-                    // System.out.print("After: ");
-                    // g.printPath(g.getTempPath());
-                    // g.printPath(g.g.getTempPath()());
-                    // ArrayList<Vertex> tempList = g.getTempPath()();
-                    // g.printPath(g.getTempPath()());
-                    // System.out.println(g.getTempPath().size());
-                    // Remove first and last elements
+                    // Remove first and last elements of this path
                     if (g.getTempPath().size() > 2) {
-                        // g.printPath(g.getTempPath());
                         g.getTempPath().remove(0);
                         g.getTempPath().remove(g.getTempPath().size() - 1);
-                        // g.printPath(g.getTempPath());
-                        // // // If no elements from this cycle are in original cycle, add to it
+                        // Check if any elements from this path are in the first cycle found
                         boolean add = true;
                         for (int j = 0; j < g.getTempPath().size(); j++) {
                             for (int k = 0; k < path.size(); k++) {
@@ -179,44 +131,20 @@ public class App {
                                 }
                             }
                         }
-
+                        // If no elements are in original cycle, add to it
                         if (add) {
                             for (int r = 0; r < g.getTempPath().size(); r++) {
                                 path.add(i + r, g.getTempPath().get(r));
                             }
                         }
-                        if (firstSize > path.size()) {
-                            System.out.print("Expanded to " + path.size() + " units: ");
-                            g.printPath(path);
-                        }
                     }
-
-                    expanded = path.size() * 5;
                 }
             }
 
         }
 
-        // boolean t = false;
-        // while (!t) {
-        // seed = rand.nextInt(teams.size());
-        // t = g.circle(teams.get(seed), size);
-        // }
         g.printPath(path);
         System.out.println(path.size());
-
-        // int sum = 0;
-        // for (int i = 0; i < teams.size(); i++) {
-        // sum = sum + teams.get(i).neighbors.size();
-        // }
-        // System.out.println(sum + "/" + teams.size());
-        // System.out.println(sum / teams.size());
-        // int t = 99;
-        // g.vertexPrint(teams.get(t));
-        // for (int i = 0; i < teams.get(t).neighbors.size(); i++) {
-        // g.vertexPrint(teams.get(t).neighbors.get(i));
-        // System.out.print(", ");
-        // }
 
     }
 }
--- src/seasonGraph.java
+++ src/seasonGraph.java
@@ -14,46 +14,44 @@ public class seasonGraph extends Graph {
     }
 
     // Finds some randomly chosen large cycle in the graph
+    // Vertex seed is the starting point for this cycle
+    // The arrayList returned has at least size elements
     public ArrayList<Vertex> circle(Vertex seed, int size) {
+        // Initialize a path to be added to
         ArrayList<Vertex> path = new ArrayList<Vertex>(vertices.size());
+        // Initialize an arrayList of vertices
         ArrayList<Vertex> avoid = new ArrayList<Vertex>();
         int last = seed.id;
         int first = seed.id;
+        // Add the seed to the path
         path.add(this.getID(last));
         int count = 0;
+        // Keep track of how many times this while loop runs
+        // Terminate after 5000 runs
         int times = 0;
         while (path.size() < size && times < 5000) {
             // Iterate through graph to find path
-            // while (times < 500) {
             Vertex k = this.getID(last);
-            // this.vertexPrint(k);
+            // Keep track of the neighbors of k that have the most neighbors
+            int[] neighborCount = new int[k.neighbors.size()];
             int lastCount = count;
             // Iterate through neighbors of this vertex
-            // Keep track of the neighboring vertices with the most neighbors
-            int[] neighborCount = new int[k.neighbors.size()];
             if (k.neighbors.size() > 0) {
                 Vertex l = k.neighbors.get(0);
                 for (int j = 0; j < k.neighbors.size(); j++) {
                     l = k.neighbors.get(j);
+                    // Keep track of how many neighbors each vertex has
                     neighborCount[j] = l.neighbors.size();
-
-                    // System.out.print(j + "th Check: ");
-                    // this.vertexPrint(k);
-                    // System.out.print("with ");
-                    // this.vertexPrint(l);
-                    // System.out.print(this.isNeighbor(k, l));
-                    // System.out.println(" ");
-
-                    // If this vertex is not on the backtracking list
+                    // If this vertex is on the backtracking list
                     if (avoid.contains(l) || (k.id == l.id)) {
                         neighborCount[j] = 0;
                     }
-                    // j++;
                 }
                 int largestIndex = 0;
                 int largestNeighbor = neighborCount[0];
                 ArrayList<Integer> seen = new ArrayList();
                 Random rand = new Random();
+                // Find the neighbor with the most neighbors
                 // Use some randomness to pick next
                 for (int n = 0; n < ((int) neighborCount.length / 2); n++) {
                     int m = rand.nextInt(neighborCount.length - 1);
@@ -76,7 +74,6 @@ public class seasonGraph extends Graph {
             }
 
             // Backtracking
-            // System.out.println(lastCount + ", " + count);
             // If nothing was added on the last pass
             if (count == lastCount) {
                 if (count > 1) {
@@ -91,24 +88,23 @@ public class seasonGraph extends Graph {
                 }
                 // break;
             } else {
+                // If vertices were added to the path, clear the avoid arrayList
                 avoid.clear();
             }
             // this.printPath(path);
             times++;
         }
 
+        // If the path is some arbitrary size
         if (path.size() > 10) {
+            // Convert the path to a cycle
             ArrayList<Vertex> endPath = pathToCycle(path.get(0), path.get(path.size() - 1));
             if (!endPath.isEmpty()) {
-                // System.out.println("\n\n");
-                // this.printPath(path);
-                // this.printPath(endPath);
                 path.addAll(endPath);
             } else
                 path = new ArrayList<Vertex>();
         } else
             path = new ArrayList<Vertex>();
-        // System.out.println(end);
         return path;
     }
 
@@ -116,32 +112,32 @@ public class seasonGraph extends Graph {
     // Use in conjunction with circle to make a cycle from a path
     public ArrayList<Vertex> pathToCycle(Vertex a, Vertex b) {
         // Use BFS from b to a
+        // Keep track of which vertices have been visited
         boolean visited[] = new boolean[this.vertices.size()];
+        // Keep a queue of vertices to visit
         LinkedList<Vertex> queue = new LinkedList<Vertex>();
+        // Keep track of parents of each vertex
         Dictionary<Vertex, Vertex> parents = new Hashtable<Vertex, Vertex>();
+        // Mark b as visited, add to front of queue
         visited[this.findIndex(b)] = true;
         queue.add(b);
 
         while (queue.size() != 0) {
-            // Take element off queue
+            // Take element off front of queue
             Vertex s = queue.poll();
             // Traverse through this element's neighbors
             for (int i = 0; i < s.neighbors.size(); i++) {
                 // If this element is a, unpack list through parents
                 if (s.neighbors.get(i).id == a.id) {
-                    // this.vertexPrint(a);
                     Vertex p = parents.get(s);
                     ArrayList<Vertex> endPath = new ArrayList<Vertex>();
                     endPath.add(s.neighbors.get(i));
                     endPath.add(s);
                     while (p.id != b.id) {
-                        // this.vertexPrint(p);
                         endPath.add(p);
                         p = parents.get(p);
-                        // this.vertexPrint(p);
                     }
-                    // System.out.println(parents);
-                    // endPath.add(b);
+                    // Reverse endPath and return
                     Collections.reverse(endPath);
                     // this.printPath(endPath);
                     return endPath;
@@ -155,58 +151,63 @@ public class seasonGraph extends Graph {
                 }
             }
         }
+        // If a path from b to a could not be found, return an empty ArrayList
         return new ArrayList<Vertex>();
 
     }
 
+    // Takes two vertices and finds a long path between them
+    // avoid is a list of vertices to not include in the path
+    // This long path is stored in this.tempPath
     public void longestPath(Vertex a, Vertex b, ArrayList<Vertex> avoid) {
         boolean visited[] = new boolean[this.vertices.size()];
+        // Mark all vertices in avoid as visited
         for (int j = 0; j < avoid.size(); j++) {
             if (!(avoid.get(j).id == a.id || avoid.get(j).id == b.id))
                 visited[this.findIndex(avoid.get(j))] = true;
         }
+        // Initialize a path and add a to the front of it
         ArrayList<Vertex> pathList = new ArrayList<Vertex>();
         pathList.add(a);
         boolean done = false;
 
+        // clear tempPath
         this.tempPath = new ArrayList<Vertex>();
         this.tempPathSize = 0;
 
         this.longestPathUtil(a, b, visited, pathList, done);
-        // System.out.print("After: ");
-        // this.printPath(this.tempPath);
-
     }
 
     public void longestPathUtil(Vertex a, Vertex b, boolean[] visited, ArrayList<Vertex> pathList, boolean done) {
+        // If b has already been connected to a, don't run
         if (done)
             return;
+
+        // If a == b
         if (a.id == b.id) {
+            // If this is the longest path found
             if (pathList.size() > this.tempPathSize) {
-                // System.out.println("Same below: ");
+                // Set this.tempPath to pathList
                 this.tempPath = new ArrayList<>(pathList);
                 done = true;
-                // this.printPath(this.tempPath);
-                // this.printPath(pathList);
             }
             return;
         }
-        // this.vertexPrint(a);
-        // this.vertexPrint(b);
+        // Mark a as visited
         visited[this.findIndex(a)] = true;
 
-        for (
-
-                int i = 0; i < a.neighbors.size(); i++) {
+        // Iterate through all neighbors of a
+        for (int i = 0; i < a.neighbors.size(); i++) {
+            // If this neighbor has not been visited
             if (!visited[this.findIndex(a.neighbors.get(i))]) {
+                // Add to list, run recursively
                 pathList.add(a.neighbors.get(i));
                 longestPathUtil(a.neighbors.get(i), b, visited, pathList, done);
-
                 pathList.remove(a.neighbors.get(i));
             }
         }
 
-        visited[this.findIndex(a)] = false;
+        // visited[this.findIndex(a)] = false;
     }
 
     public ArrayList<Vertex> getTempPath() {
@@ -220,7 +221,7 @@ public class seasonGraph extends Graph {
             if (i + 1 == path.size()) {
                 System.out.println("]");
             } else {
-                System.out.print(", ");
+                System.out.print(" --> ");
             }
         }
     }
